8ab35fa5ee96947aafd7428c9f2b03c5e879deab
kenve <zwei.xie@gmail.com>
2023-01-20 12:26:13 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,82 @@
+{
+  "log": {
+    "loglevel": "warning",
+    "access": "/home/vpsadmin/xray_log/access.log",
+    "error": "/home/vpsadmin/xray_log/error.log"
+  },
+  "dns": {
+    "servers": [
+      "https+local://1.1.1.1/dns-query",
+      "localhost"
+    ]
+  },
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "block"
+      },
+      {
+        "type": "field",
+        "ip": ["geoip:cn"],
+        "outboundTag": "block"
+      },
+      {
+        "type": "field",
+        "domain": [
+          "geosite:category-ads-all"
+        ],
+        "outboundTag": "block"
+      }
+    ]
+  },
+  "inbounds": [
+    {
+      "port": 443,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "982d329d-7ecf-4c9a-84c4-dd8f84cc3dfd",
+            "flow": "xtls-rprx-vision",
+            "level": 0,
+            "email": "zwei.xie@gmail.com"
+          }
+        ],
+        "decryption": "none",
+        "fallbacks": [
+          {
+            "dest": 80
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "tls",
+        "tlsSettings": {
+          "alpn": "http/1.1",
+          "certificates": [
+            {
+              "certificateFile": "~/xray_cert/xray.crt",
+              "keyFile": "~/xray_cert/xray.key"
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "tag": "direct",
+      "protocol": "freedom"
+    },
+    {
+      "tag": "block",
+      "protocol": "blackhole"
+    }
+  ]
+}
